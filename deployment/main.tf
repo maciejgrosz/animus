ce1a7559d41942d671916385804095c75119ba9c
maciejgrosz <maciej.groszyk8@gmail.com>
2025-04-14 22:20:45 +0200

    add .nvmrc

--- deployment/main.tf
+++ deployment/main.tf
@@ -2,20 +2,24 @@ resource "aws_amplify_app" "animus_app" {
   name                = "animus"
   repository          = "https://github.com/maciejgrosz/animus"  # Replace with your GitHub repo URL
   build_spec          = <<EOF
-version: 1
-applications:
-  - app_root: /
-    frontend:
-      phases:
-        build:
-          commands:
-            - npm install
-            - npm run build
-      artifacts:
-        base_directory: /dist
-        files:
-          - '**/*'
-  EOF
+  version: 1
+  frontend:
+    phases:
+      preBuild:
+        commands:
+          - npm ci
+      build:
+        commands:
+          - npm run build
+    artifacts:
+      baseDirectory: dist
+      files:
+        - '**/*'
+    cache:
+      paths:
+        - node_modules/**/*
+
+    EOF
   environment_variables = {
     NODE_ENV = "production"
   }
@@ -28,14 +32,4 @@ resource "aws_amplify_branch" "main_branch" {
   branch_name = "main"
   enable_auto_build = true
 }
-#
-# # Output Amplify App ARN (ID) and URL
-# output "amplify_app_arn" {
-#   description = "The ARN of the Amplify app"
-#   value       = aws_amplify_app.animus_app.app_arn   # Corrected to app_arn
-# }
-#
-# output "amplify_app_url" {
-#   description = "The URL of the Amplify app"
-#   value       = "https://${aws_amplify_app.animus_app.name}.amplifyapp.com"  # Construct URL manually
-# }
+
