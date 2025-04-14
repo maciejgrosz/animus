5bddf819e33bec89ae98148fdd7f3429aeda7608
maciejgrosz <maciej.groszyk8@gmail.com>
2025-04-14 22:43:24 +0200

    fix: add vite to devDependencies

--- deployment/main.tf
+++ deployment/main.tf
@@ -1,35 +1,45 @@
 resource "aws_amplify_app" "animus_app" {
-  name                = "animus"
-  repository          = "https://github.com/maciejgrosz/animus"  # Replace with your GitHub repo URL
-  build_spec          = <<EOF
-  version: 1
-  frontend:
-    phases:
-      preBuild:
-        commands:
-          - curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-          - export NVM_DIR=$HOME/.nvm
-          - source $NVM_DIR/nvm.sh
-          - nvm install 20
-          - nvm use 20
-          - node -v
-          - npm -v
-          - npm ci
-      build:
-        commands:
-          - npm run build
-    artifacts:
-      baseDirectory: dist
-      files:
-        - '**/*'
-    cache:
-      paths:
-        - node_modules/**/*
-    EOF
+  name       = "animus"
+  repository = "https://github.com/maciejgrosz/animus"
+
+  build_spec = <<EOF
+version: 1
+frontend:
+  phases:
+    preBuild:
+      commands:
+        - bash -c '
+            curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash &&
+            export NVM_DIR=$HOME/.nvm &&
+            source $NVM_DIR/nvm.sh &&
+            nvm install 20 &&
+            nvm use 20 &&
+            node -v &&
+            npm -v &&
+            npm ci
+          '
+    build:
+      commands:
+        - bash -c '
+            export NVM_DIR=$HOME/.nvm &&
+            source $NVM_DIR/nvm.sh &&
+            nvm use 20 &&
+            npm run build
+          '
+  artifacts:
+    baseDirectory: dist
+    files:
+      - '**/*'
+  cache:
+    paths:
+      - node_modules/**/*
+EOF
+
   environment_variables = {
     NODE_ENV = "production"
   }
 }
+
 
 
 # Create a branch for automatic deployments
